ecad372e3c63250abe7599e9f9aa0337023e0cd8
Joe Cooper <josephcooper3@gmail.com>
2019-09-18 10:51:40 +0100

    adds Index route in back end

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,4 +3,26 @@ const ObjectId = require('mongodb').ObjectId;
 
 const createRouter = function (collection) {
 
-}
+  const router = express.Router();
+
+  //INDEX
+  router.get('/', (req, res) => {
+    collection.find().toArray()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+      console.error(error);
+      res.status(500);
+      res.json({status: 500, error: err});
+    })
+  })
+
+  //CREATE
+
+
+  //DELETE
+
+
+  return router;
+};
+
+module.exports = createRouter;
