4ea12cdca3b72b239e307e8377477cda6971ff6b
Joe Cooper <josephcooper3@gmail.com>
2019-09-18 13:20:25 +0100

    adds table-component

--- client/src/components/TableItem.vue
+++ client/src/components/TableItem.vue
@@ -1,10 +1,21 @@
 <template>
-  
+  <tr>
+    <td>{{booking.name}}</td>
+    <td>{{booking.email}}</td>
+    <td>{{booking.checkedIn}}</td>
+    <td>
+      <button>Delete</button>
+    </td>
+  </tr>
 </template>
 
 <script>
-export default {
+import BookingService from '../services/BookingService';
+import { eventBus } from '../main';
 
+export default {
+  name: "table-item",
+  props: ["booking"]
 }
 </script>
 
